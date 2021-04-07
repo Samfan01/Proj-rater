24a61feaccb7964775c3098219195b6cb51ecd20
samfan01 <dsamfan@gmail.com>
2021-04-07 21:44:20 +0300

    created the new project view to direct users to the new project form

--- rater/views.py
+++ rater/views.py
@@ -8,3 +8,17 @@ def home(request):
     template_name = 'home.html'
     title = 'Project-Rator : Where You Rate and get Your Project Rated on.'
     return render(request,template_name,{'title':title,'projects':projects})
+
+def new_project(request):
+    model = Project
+    form = NewProjectForm
+    template_name = 'new_project.html',
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        
+        return render(request,template_name,{'form':form})
+    
