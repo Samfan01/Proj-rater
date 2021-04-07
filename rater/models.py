554ad7278cb171ddb0d33d3d86ba0ddae3fb8329
samfan01 <dsamfan@gmail.com>
2021-04-07 20:08:46 +0300

    created the project model for creating new instances of project

--- rater/models.py
+++ rater/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    '''
+    Project class to create instances of the project post
+    '''
+    title = models.CharField(max_length = 30)
+    image = models.ImageField(upload_to = 'rator/')
+    description = models.TextField()
+    link = models.URLField()
+    
+    def __str__(self):
+        return self.title
+        
+    
+    
+    
