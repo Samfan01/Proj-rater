530267eaa565e8ab151a767c7877c3cac0cd81a5
samfan01 <dsamfan@gmail.com>
2021-04-08 11:05:48 +0300

    ctreated the profileform that will help in adding profile information

--- rater/forms.py
+++ rater/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from . models import Project
+from . models import Project,Profile
 
 
 class NewProjectForm(forms.ModelForm):
@@ -11,4 +11,14 @@ class NewProjectForm(forms.ModelForm):
             'title':forms.TextInput(attrs = {'class':'form-control'}),
             'description':forms.TextInput(attrs = {'class':'form-control'}),
             'link':forms.TextInput(attrs = {'class':'form-control'}),
+        }
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('email','bio','profile_pic')
+        
+        widgets = {
+            'email': forms.TextInput(attrs = {'class': 'form-control'}),
+            'bio': forms.Textarea(attrs = {'class': 'form-control'}),
         }
